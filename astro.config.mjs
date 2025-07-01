948334292e8f42634907de1e2af9f6c8f47fccd3
Maciejj1 <maciekwawryszuk@gmail.com>
2025-07-01 17:07:01 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://maciejj1.github.io', // <--- Ustaw tu swój adres strony
-  base: '/dgklogistics', // <--- Ustaw bazowy URL, jeśli jest inny
+  base: '/dgklogistics-preview', // <--- Ustaw bazowy URL, jeśli jest inny
   integrations: [tailwind()],
   redirects: {
-    "/": "/nl"
+    '/dgklogistics-preview/': '/dgklogistics-preview/nl',
+    '/': '/dgklogistics-preview/nl', // Redirect root to the Dutch version
   },
   i18n: {
     locales: ["nl", "pl", "de", "en"], // Locales you want to support
